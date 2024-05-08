20a31f4db9eabfe1780b6e4d3e0d1c7185329f92
Rakesh Patel <107678438+RAKESH-PATEL57@users.noreply.github.com>
2024-05-08 11:17:00 +0530

    Initialized

---
+++ app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+
+const port = 5000 || process.env.PORT;
+
+
+//helps to pass data through forms and pages.
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Static files
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+// app.get('/', function(req, res){
+//     const locals = {
+//         title: 'Notes',
+//         description: 'Free Notes App'
+//     }
+//     res.render('index', locals);
+// });
+
+//Routes
+app.use('/', require('./server/routes/index'));
+
+
+app.listen(port, () => {
+    console.log(`App listening on port http://localhost:${port}`);
+});
