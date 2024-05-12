967cbdb91ea298e44c93a05937b94faa7db209ee
Rakesh Patel <107678438+RAKESH-PATEL57@users.noreply.github.com>
2024-05-12 09:50:53 +0530

    Update mainController.js

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -4,7 +4,10 @@ exports.homepage = async (req, res) => {
         title: "Notes",
         description: "Free Notes App",
     };
-    res.render('index', locals);
+    res.render('index', {
+        locals,
+        layout: '../views/layouts/front-page'
+    });
 }
 
 
