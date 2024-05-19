c8bdcc1ddd0077d628391301f0a2314b5fdb84a4
Rakesh Patel <107678438+RAKESH-PATEL57@users.noreply.github.com>
2024-05-19 22:35:59 +0530

    added auth

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const app = express();
+
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db');
 
+const app = express();
 const port = 5000 || process.env.PORT;
 
+app.use(passport.initialize());
+app.use(express.json());
 
 //helps to pass data through forms and pages.
 app.use(express.urlencoded({extended: true}));
@@ -34,6 +40,7 @@ app.set('view engine', 'ejs');
 // });
 
 //Routes
+app.use('/', require('./server/routes/auth'));
 app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/dashboard'));
 
