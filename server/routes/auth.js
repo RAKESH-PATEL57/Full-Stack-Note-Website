a5a1c10bd144fdbe2abb6e7a111534f42be8f51f
Rakesh Patel <107678438+RAKESH-PATEL57@users.noreply.github.com>
2024-05-20 13:56:07 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+//Google Login Route
+router.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+// Retrive user data
+router.get('/google/callback', 
+  passport.authenticate('google', { 
+    failureRedirect: '/login-failure',
+    successRedirect: '/dashboard' 
+})
+);
+
+// Route if something goes wrong
+router.get('/login-failure', (req, res) => {
+    res.send('Something went wrong');
+})
 
 module.exports = router;
