b0ccae471428e7f14e90c124d8d0dd5e1571b4cf
Rakesh Patel <107678438+RAKESH-PATEL57@users.noreply.github.com>
2024-05-22 12:34:46 +0530

    linked User

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const User = require('../models/User');
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
@@ -10,15 +10,13 @@ passport.use(new GoogleStrategy({
     callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
   function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+     console.log(profile);
   }
 ));
 
 //Google Login Route
 router.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile'] }));
+  passport.authenticate('google', { scope: ['email','profile'] }));
 
 // Retrive user data
 router.get('/google/callback', 
@@ -31,6 +29,19 @@ router.get('/google/callback',
 // Route if something goes wrong
 router.get('/login-failure', (req, res) => {
     res.send('Something went wrong');
+});
+
+// Presist user data after successful authentication 
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+// Retrieve user data from session
+passport.deserializeUser(function(id, done) {
+    User.FindById(id, function(err, user) {
+        done(err, user);
+    })
 })
+
 
 module.exports = router;
