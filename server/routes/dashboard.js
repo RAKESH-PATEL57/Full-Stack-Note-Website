a58846b56ed7d1b8c7eae74dcc8d978fba00a170
Rakesh Patel <107678438+RAKESH-PATEL57@users.noreply.github.com>
2024-05-27 22:09:17 +0530

    Update dashboard.js

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const router = require('router');
+const router = express.Router('router');
+const { isLoggedIn } = require('../middleware/checkAuth');
 const dashboardController = require('../controllers/dashboardController');
 
 
 // DashBoard Routes
-router.get('/dashboard', dashboardController.dashboard);
+// router.get('/dashboard',dashboardController.dashboard);
+router.get('/dashboard', isLoggedIn, dashboardController.dashboard);
 
 module.exports = router;
 
